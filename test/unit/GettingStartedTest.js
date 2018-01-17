fdb30ea33c3be44ae97af2703e1263fe7f4a01ce
clement <cleement.l@gmail.com>
2018-01-17 15:25:03 +0100

    first commit

---
+++ test/unit/GettingStartedTest.js
@@ -0,0 +1,14 @@
+/*function add(a,b){
+		return a+b;
+}
+
+describe ("add",function(){
+	
+	it("test",function(){
+		var a=0;
+		var b=0;
+		var res = add(a,b);
+		expect(res).toEqual(0);
+	});
+	
+});*/
---
+++ test/unit/UtilTest.js
@@ -0,0 +1,65 @@
+describe ("factorial",function(){
+	
+	    var testCase = [
+	            {
+	                n : 0,
+	                factorial : 1
+	            },
+	            {
+	                n : 1,
+	                factorial : 1
+	            },
+	            {
+	                n : 2,
+	                factorial : 2
+	            },
+	            {
+	                n : 3,
+	                factorial : 6
+	            },
+	            {
+	                n : 4,
+	                factorial : 24
+	            },
+	        ];
+	    
+	    testCase.forEach(function(testCase) {
+	        it("should returns " + testCase.factorial + " when n = " + testCase.n, function() {
+	            var result = Util.factorial(testCase.n);
+	            expect(result).toEqual(testCase.factorial);
+	        });
+	    });
+	});
+
+describe ("arrangement",function(){
+	
+    var testCase = [
+            {
+                n : 0,
+                r : 0,
+                arrangement : 1
+            },
+            {
+                n : 1,
+                r : 1,
+                arrangement : 1
+            },
+            {
+                n : 2,
+                r : 1,
+                arrangement : 2
+            },
+            {
+                n : 5,
+                r : 2,
+                arrangement : 20
+            },
+        ];
+    
+    testCase.forEach(function(testCase) {
+        it("should returns " + testCase.arrangement + " when n = " + testCase.n + " and r = " + testCase.r, function() {
+            var result = Util.arrangement(testCase.n,testCase.r);
+            expect(result).toEqual(testCase.arrangement);
+        });
+    });
+});
